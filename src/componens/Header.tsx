68a6100bd7d5a37c9b2da62ab589418f20d75404
Lev <leozolotukhin@gmail.com>
2025-08-25 13:55:43 +0200

    footer + header distance h1 p

--- src/componens/Header.tsx
+++ src/componens/Header.tsx
@@ -3,7 +3,7 @@
 function Header() {
 	return (
 		<header className="w-full text-center mb-8">
-			<h1 className="artist text-4xl font-bold sm:text-5xl md:text-6xl">
+			<h1 className="headline text-4xl font-bold sm:text-5xl md:text-6xl">
 				To Be Splashed
 			</h1>
 			<p className="artist text-lg mt-2 sm:text-xl">
